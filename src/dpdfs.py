17af7472aefdd802713bbb3cecceb9c09904dca7
Derek Anderson <public@kered.org>
2013-03-03 14:22:54 -0600

    now writing to hg

--- src/dpdfs.py
+++ src/dpdfs.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
 
-import os, errno, sys, threading, collections, uuid, shutil
+import os, errno, sys, threading, collections, uuid, shutil, traceback
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 import libtorrent as lt
+import hgapi as hg
 
 class TmpFile(object):
   def __init__(self, path):
     self.path = path
 
 class DPDFS(LoggingMixIn, Operations):
-  def __init__(self, root):
+  def __init__(self, name, root):
+    self.name = name
     self.root = os.path.realpath(root)
     self.hgdb = os.path.join(self.root, 'hgdb')
     self.tmp = os.path.join(self.root, 'tmp')
     self.dat = os.path.join(self.root, 'dat')
     self.shadow = os.path.join(self.root, 'shadow')
+    
     if not os.path.isdir(self.hgdb): os.makedirs(self.hgdb)
+    self.repo = hg.Repo(self.hgdb)
+    vfn = os.path.join(self.root, '__volume__')
+    if os.path.isfile(vfn):
+      with open(vfn,'r') as f:
+        existing_name = f.read().strip()
+        if self.name != existing_name:
+          raise Exception('volume name "%s" != existing volume name "%s"' % (self.name, existing_name))
+    else:
+      with open(vfn,'w') as f:
+        f.write(self.name)
+      self.repo.hg_init()
+    
     if not os.path.isdir(self.tmp): os.makedirs(self.tmp)
     if not os.path.isdir(self.dat): os.makedirs(self.dat)
     if not os.path.isdir(self.shadow): os.makedirs(self.shadow)
@@ -25,6 +40,8 @@ class DPDFS(LoggingMixIn, Operations):
 
   def __call__(self, op, path, *args):
     print op, path, ('...data...' if op=='write' else args)
+    if path.startswith('/.Trash'): raise FuseOSError(errno.EACCES)
+    if path.endswith('/.__dpdfs_dir__'): raise FuseOSError(errno.EACCES)
     return super(DPDFS, self).__call__(op, path, *args)
 
   def access(self, path, mode):
@@ -35,15 +52,21 @@ class DPDFS(LoggingMixIn, Operations):
 #  chown = os.chown
 
   def create(self, path, mode):
-    tmp = uuid.uuid4().hex
-    self.open_files[path] = tmp
-    return os.open(os.path.join(self.tmp, tmp), os.O_WRONLY | os.O_CREAT, mode)
+    with self.rwlock:
+      tmp = uuid.uuid4().hex
+      self.open_files[path] = tmp
+      with open(self.hgdb+path,'wb') as f:
+        pass
+      self.repo.hg_add(self.hgdb+path)
+      return os.open(os.path.join(self.tmp, tmp), os.O_WRONLY | os.O_CREAT, mode)
 
   def flush(self, path, fh):
-        return os.fsync(fh)
+    with self.rwlock:
+      return os.fsync(fh)
 
   def fsync(self, path, datasync, fh):
-        return os.fsync(fh)
+    with self.rwlock:
+      return os.fsync(fh)
 
   def getattr(self, path, fh=None):
     st_size = None
@@ -64,13 +87,21 @@ class DPDFS(LoggingMixIn, Operations):
   getxattr = None
   
   def link(self, target, source):
-        return os.link(source, target)
+    with self.rwlock:
+      return os.link(source, target)
 
   listxattr = None
 #  mknod = os.mknod
 
   def mkdir(self, path, flags):
-    return os.mkdir(self.hgdb+path, flags)
+    with self.rwlock:
+      fn = self.hgdb+path
+      ret = os.mkdir(fn, flags)
+      with open(fn+'/.__dpdfs_dir__','w') as f:
+        f.write("hg doesn't track empty dirs, so we add this file...")
+      self.repo.hg_add(fn+'/.__dpdfs_dir__')
+      self.repo.hg_commit('mkdir %s' % path, files=[fn+'/.__dpdfs_dir__'])
+      return ret
 
   def read(self, path, size, offset, fh):
     with self.rwlock:
@@ -78,66 +109,87 @@ class DPDFS(LoggingMixIn, Operations):
       return os.read(fh, size)
 
   def open(self, path, flags):
-    if not (flags & (os.O_WRONLY | os.O_RDWR | os.O_APPEND | os.O_CREAT | os.O_EXCL | os.O_TRUNC)):
-      print '\treadonly'
-      t = get_torrent_dict(self.hgdb+path)
-      if t:
-        name = t['info']['name']
-        return os.open(os.path.join(self.dat, name[:2], name), flags)
-      else:
-        return os.open(self.hgdb+path, flags)
-    tmp = uuid.uuid4().hex
-    fn = self.hgdb+path
-    if os.path.isfile(fn):
-      with open(fn, 'rb') as f:
-        prev = lt.bdecode(f.read())['info']['name']
-        prev_fn = os.path.join(self.dat, prev[:2], prev)
-        if os.path.isfile(prev_fn):
-          shutil.copyfile(prev_fn, os.path.join(self.tmp, tmp))
-    self.open_files[path] = tmp
-    return os.open(os.path.join(self.tmp, tmp), flags)
-    return 0
+    with self.rwlock:
+      fn = self.hgdb+path
+      if not (flags & (os.O_WRONLY | os.O_RDWR | os.O_APPEND | os.O_CREAT | os.O_EXCL | os.O_TRUNC)):
+        #print '\treadonly'
+        t = get_torrent_dict(fn)
+        if t:
+          name = t['info']['name']
+          return os.open(os.path.join(self.dat, name[:2], name), flags)
+        else:
+          return os.open(fn, flags)
+      tmp = uuid.uuid4().hex
+      if os.path.isfile(fn):
+        with open(fn, 'rb') as f:
+          prev = lt.bdecode(f.read())['info']['name']
+          prev_fn = os.path.join(self.dat, prev[:2], prev)
+          if os.path.isfile(prev_fn):
+            shutil.copyfile(prev_fn, os.path.join(self.tmp, tmp))
+      self.open_files[path] = tmp
+      return os.open(os.path.join(self.tmp, tmp), flags)
+      return 0
   
     
   def readdir(self, path, fh):
-    return ['.', '..'] + os.listdir(self.hgdb+path)
+    with self.rwlock:
+      return ['.', '..'] + [x for x in os.listdir(self.hgdb+path) if x!=".__dpdfs_dir__" and x!='.hg']
 
 #    readlink = os.readlink
 
   def release(self, path, fh):
-    ret = os.close(fh)
-    if path in self.open_files:
-      self.finalize(path, self.open_files[path])
-      del self.open_files[path]
-    return ret
+    with self.rwlock:
+      ret = os.close(fh)
+      if path in self.open_files:
+        self.finalize(path, self.open_files[path])
+        del self.open_files[path]
+      return ret
     
   def finalize(self, path, uid):
-    print 'finalize', path, uid
-    fs = lt.file_storage()
-    tmp_fn = os.path.join(self.tmp, uid)
-    st_size = os.stat(tmp_fn).st_size
-    print tmp_fn, st_size
-    lt.add_files(fs, tmp_fn, st_size)
-    t = lt.create_torrent(fs)
-    #t.set_creator("dpdfs");
-    print self.tmp
-    lt.set_piece_hashes(t, self.tmp)
-    print t.generate()
-    with open(self.hgdb+path, 'wb') as f:
-      f.write(lt.bencode(t.generate()))
+    #print 'finalize', path, uid
+    try:
+      fs = lt.file_storage()
+      tmp_fn = os.path.join(self.tmp, uid)
+      try: st_size = os.stat(tmp_fn).st_size
+      except:
+        traceback.print_exc()
+        return
+        
+      #print tmp_fn, st_size
+      lt.add_files(fs, tmp_fn, st_size)
+      t = lt.create_torrent(fs)
+      #t.set_creator("dpdfs");
+      lt.set_piece_hashes(t, self.tmp)
+      tdata = t.generate()
+      #print tdata
+      with open(self.hgdb+path, 'wb') as f:
+        f.write(lt.bencode(tdata))
+      #print 'wrote', self.hgdb+path
+      dat_dir = os.path.join(self.dat, uid[:2])
+      if not os.path.isdir(dat_dir): 
+        try: os.mkdir(dat_dir)
+        except: pass
+      os.rename(tmp_fn, os.path.join(dat_dir, uid))
+      if os.path.exists(self.shadow+path): os.remove(self.shadow+path)
+      os.symlink(os.path.join(dat_dir, uid), self.shadow+path)
+      #print 'committing', self.hgdb+path
+      self.repo.hg_commit('wrote %s' % path, files=[self.hgdb+path])
+    except Exception as e:
+      traceback.print_exc()
+      raise e
     
-    dat_dir = os.path.join(self.dat, uid[:2])
-    os.makedirs(dat_dir)
-    os.rename(tmp_fn, os.path.join(dat_dir, uid))
-    os.symlink(os.path.join(dat_dir, uid), self.shadow+path)
-
   def rename(self, old, new):
-    return os.rename(self.hgdb+old, self.hgdb+new)
+    with self.rwlock:
+      self.repo.hg_rename(self.hgdb+old, self.hgdb+new)
+      self.repo.hg_commit('rename %s to %s' % (old,new), files=[self.hgdb+old, self.hgdb+new])
 
 #  rmdir = os.rmdir
 
-  def rmdir(self, path, flags):
-    return os.rmdir(self.hgdb+path, flags)
+  def rmdir(self, path):
+    with self.rwlock:
+      self.repo.hg_remove(self.hgdb+path+'/.__dpdfs_dir__')
+      self.repo.hg_commit('rmdir %s' % path, files=[self.hgdb+path+'/.__dpdfs_dir__'])
+      #return os.rmdir(self.hgdb+path)
 
   def statfs(self, path):
         stv = os.statvfs(self.hgdb+path)
@@ -146,21 +198,27 @@ class DPDFS(LoggingMixIn, Operations):
             'f_frsize', 'f_namemax'))
 
   def symlink(self, target, source):
-        return os.symlink(source, target)
+    with self.rwlock:
+      ret = os.symlink(source, target)
+      return ret
+        
 
   def truncate(self, path, length, fh=None):
-    with open(os.path.join(self.tmp, self.open_files[path]), 'r+') as f:
-            f.truncate(length)
+    with self.rwlock:
+      with open(os.path.join(self.tmp, self.open_files[path]), 'r+') as f:
+        f.truncate(length)
 
 #  utimens = os.utime
 
   def unlink(self, path):
-    return os.unlink(self.hgdb+path)
+    with self.rwlock:
+      self.repo.hg_remove(self.hgdb+path)
+      self.repo.hg_commit('unlink %s' % path, files=[self.hgdb+path])
 
   def write(self, path, data, offset, fh):
-        with self.rwlock:
-            os.lseek(fh, offset, 0)
-            return os.write(fh, data)
+    with self.rwlock:
+      os.lseek(fh, offset, 0)
+      return os.write(fh, data)
 
 
 def get_torrent_dict(fn):
@@ -170,9 +228,9 @@ def get_torrent_dict(fn):
 
 
 if __name__ == '__main__':
-  if len(sys.argv) != 3:
-    print('usage: %s <root> <mountpoint>' % sys.argv[0])
+  if len(sys.argv) != 4:
+    print('usage: %s <name> <root> <mountpoint>' % sys.argv[0])
     sys.exit(1)
 
-  fuse = FUSE(DPDFS(sys.argv[1]), sys.argv[2], foreground=True)
+  fuse = FUSE(DPDFS(sys.argv[1], sys.argv[2]), sys.argv[3], foreground=True)
 
---
+++ src/hgapi.py
@@ -0,0 +1,364 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, with_statement
+from subprocess import Popen, STDOUT, PIPE
+try:
+    from urllib import unquote
+except: #python 3
+    from urllib.parse import unquote
+import re
+import os.path
+import os
+try:
+    import json #for reading logs
+except:
+    import simplejson as json
+
+
+class Revision(object):
+    """A representation of a revision.
+    Available fields are::
+
+      node, rev, author, branch, parents, date, tags, desc
+
+    A Revision object is equal to any other object with the same value for node
+    """
+    def __init__(self, json_log):
+        """Create a Revision object from a JSON representation"""
+        rev = json.loads(json_log)
+        
+        for key in rev.keys():
+            self.__setattr__(key, unquote(rev[key]))
+        self.rev = int(self.rev)
+        if not self.branch: self.branch='default'
+        if not self.parents:
+            self.parents = [int(self.rev)-1]
+        else:
+            self.parents = [int(p.split(':')[0]) for p in self.parents.split()]
+
+    def __iter__(self):
+        return self
+
+    def __eq__(self, other):
+        """Returns true if self.node == other.node"""
+        return self.node == other.node
+
+class Repo(object):
+    """A representation of a Mercurial repository"""
+    def __init__(self, path, user=None):
+        """Create a Repo object from the repository at path"""
+        self.path = path
+        self.cfg = False
+        self.user = user
+        self._env = os.environ
+        self._env['LANG'] = 'en_US'
+ 
+    def __getitem__(self, rev=slice(0, 'tip')):
+        """Get a Revision object for the revision identifed by rev
+           rev can be a range (6c31a9f7be7ac58686f0610dd3c4ba375db2472c:tip)
+           a single changeset id
+           or it can be left blank to indicate the entire history
+        """
+        if isinstance(rev, slice):
+            return self.revisions(rev)
+        return self.revision(rev)
+
+    def hg_command(self, *args):
+        """Run a hg command in path and return the result.
+        Throws on error."""    
+        proc = Popen(["hg", "--cwd", self.path, "--encoding", "UTF-8"] + list(args), stdout=PIPE, stderr=PIPE, env=self._env)
+
+        out, err = [x.decode("utf-8") for x in  proc.communicate()]
+        print (" ".join(["hg",] + ['"%s"'%x if ' ' in x else x for x in args]))
+
+        if proc.returncode:
+            cmd = (" ".join(["hg", "--cwd", self.path] + list(args)))
+            print(cmd)
+            raise Exception("Error running %s:\n\tErr: %s\n\tOut: %s\n\tExit: %s" 
+                            % (cmd,err,out,proc.returncode))
+        return out
+
+    def hg_init(self):
+        """Initialize a new repo"""
+        self.hg_command("init")
+
+    def hg_id(self):
+        """Get the output of the hg id command (truncated node)"""
+        res = self.hg_command("id", "-i")
+        return res.strip("\n +")
+        
+    def hg_rev(self):
+        """Get the revision number of the current revision"""
+        res = self.hg_command("id", "-n")
+        str_rev = res.strip("\n +")
+        return int(str_rev)
+
+    def hg_add(self, filepath):
+        """Add a file to the repo"""
+        self.hg_command("add", filepath)
+
+    def hg_remove(self, filepath, force=False):
+        """Remove a file from the repo"""
+        cmd = ["remove", str(filepath)]
+        if force: cmd.append("-f")
+        cmd.append(str(filepath))
+        self.hg_command(*cmd)
+
+    def hg_rename(self, src, dest):
+        """Rename a file in the repo"""
+        cmd = ["rename", str(src), str(dest)]
+        self.hg_command(*cmd)
+
+    def hg_update(self, reference, clean=False):
+        """Update to the revision identified by reference"""
+        cmd = ["update", str(reference)]
+        if clean: cmd.append("--clean")
+        self.hg_command(*cmd)
+
+    def hg_tag(self, *tags, **kwargs):
+        """Add one or more tags to the current revision, or revision given by
+        passing 'rev' as a keyword argument::
+          
+          >>> repo.hg_tag('mytag', rev=3)"""
+        rev = kwargs.get('rev')
+        cmd = ['tag'] + list(tags)
+        if rev: cmd += ['-r', str(rev)]
+        self.hg_command(*cmd)
+
+    def hg_tags(self):
+        """Get all tags from the repo as 
+        a dict containing tag: shortnode mapping"""
+        cmd = ['tags']
+        output = self.hg_command(*cmd)
+        res = {}
+        for row in output.strip().split('\n'):
+            tag, changeset = row.split() # tip  5:aaaaaaaaaa
+            res[tag] = changeset.split(':')[1]
+        return res
+
+    def hg_heads(self, short=False):
+        """Gets a list with the node id:s of all open heads.
+        If short is given and is not False, return the short form of the node id"""
+        template = "{node}\n" if not short else "{node|short}\n"
+        res = self.hg_command("heads","--template", template)
+        return [head for head in res.split("\n") if head]
+
+    def hg_merge(self, reference, preview=False):
+        """Merge reference to current, or with 'preview' set to True get a list 
+        of revision numbers containing all revisions that would have been merged"""
+        if not preview:
+            return self.hg_command("merge", reference)
+        else:
+            revno_re = re.compile('^changeset: (\d+):\w+$')
+            out = self.hg_command("merge", "-P", reference)
+            revs = []
+            for row in out:
+                match = revno_re.match(row)
+                if match: revs.append(match.group(1)) 
+            return rev
+        
+    def hg_revert(self, all=False, *files):
+        """Revert repository"""        
+        if all:
+            cmd = ["revert", "--all"]
+        else:
+            cmd = ["revert"] + list(files)
+        self.hg_command(*cmd)
+
+    def hg_node(self):
+        """Get the full node id of the current revision"""
+        res = self.hg_command("log", "-r", self.hg_id(), "--template", "{node}")
+        return res.strip()
+
+    def hg_commit(self, message, user=None, date=None, files=[], close_branch=False):
+        """Commit changes to the repository."""
+        userspec = "-u" + user if user else "-u" + self.user if self.user else ""
+        datespec = "-d" + date if date else ""
+        close = "--close-branch" if close_branch else ""
+        args = [close, userspec, datespec] + files
+        # don't send a "" arg for userspec or close, which HG will
+        # consider the files arg, committing all files instead of what
+        # was passed in files kwarg
+        args = [arg for arg in args if arg]
+        self.hg_command("commit", "-m", message, *args)
+
+    def hg_log(self, identifier=None, limit=None, template=None, branch=None, **kwargs):
+        """Get repositiory log."""
+        cmds = ["log"]
+        if identifier: cmds += ['-r', str(identifier)]
+        if branch: cmds += ['-b', str(branch)]
+        if limit: cmds += ['-l', str(limit)]
+        if template: cmds += ['--template', str(template)]
+        if kwargs:
+            for key in kwargs:
+                cmds += [key, kwargs[key]]
+        return self.hg_command(*cmds)
+
+    def hg_branch(self, branch_name=None):
+        """ Creates a branch of branch_name isn't None
+            If not, returns the current branch name.
+        """
+        args = []
+        if branch_name:
+            args.append(branch_name)
+        branch = self.hg_command("branch", *args)
+        return branch.strip()
+
+    def get_branches(self):
+        """ Returns a list of branches from the repo, including versions """
+        branches = self.hg_command("branches")
+        branch_list = branches.strip().split("\n")
+        values = []
+        for branch in branch_list:
+            b = branch.partition(" ")
+            if not b:
+                continue
+            name = b[0].strip()
+            version = b[-1].strip()
+            values.append({'name':name, 'version':version})
+        return values
+
+    def get_branch_names(self):
+        """ Returns a list of branch names from the repo. """
+        branches = self.hg_command("branches")
+        branch_list = branches.strip().split("\n")
+        values = []
+        for branch in branch_list:
+            b = branch.partition(" ")
+            if not b:
+                continue
+            name = b[0]
+            if name:
+                name = name.strip()
+                values.append(name)
+        return values
+
+    def hg_diff(self, rev_a=None, rev_b=None, filenames=None):
+        """Get a unified diff as returned by 'hg diff'
+        rev_a and rev_b are passed as -r <rev> arguments to the call,
+        and filenames are expected to be an iterable of file names.
+
+        Returns a list of dicts where every dict has a 'filename' and 'diff' field,
+        with diff being the complete diff for the file including header (diff -r xxxx -r xxx...).
+        """
+
+        cmds = ['diff']
+        for rev in (rev_a, rev_b):
+            if not rev is None:
+                cmds += ['-r', rev]
+
+        if not filenames is None:
+            cmds += list(filenames)
+
+        result =  self.hg_command(*cmds)
+        diffs = []
+        filere = re.compile("^diff .* (\S+)$")
+        for line in result.split('\n'):
+           
+            match = filere.match(line)
+            if match:
+                diffs.append({'filename': match.groups()[0],  'diff': ''})
+            diffs[-1]['diff'] += line + '\n'
+        return diffs
+
+    def hg_status(self, empty=False, clean=False):
+        """Get repository status.
+        Returns a dict containing a *change char* -> *file list* mapping, where 
+        change char is in::
+
+         A, M, R, !, ?
+
+        Example - added one.txt, modified a_folder/two.txt and three.txt::
+
+         {'A': ['one.txt'], 'M': ['a_folder/two.txt', 'three.txt'],
+         '!': [], '?': [], 'R': []}
+
+        If empty is set to non-False value, don't add empty lists.
+        If clean is set to non-False value, add clean files as well (-A)
+        """
+        cmds = ['status']
+        if clean: cmds.append('-A')        
+        out = self.hg_command(*cmds).strip()
+        #default empty set
+        if empty:
+            changes = {}
+        else:
+            changes = {'A': [], 'M': [], '!': [], '?': [], 'R': []}
+            if clean:
+                changes['C'] = []
+
+        if not out: return changes
+        lines = out.split("\n")
+        status_split = re.compile("^(.) (.*)$")
+
+        for change, path in [status_split.match(x).groups() for x in lines]:
+            changes.setdefault(change, []).append(path)
+        return changes
+        
+    rev_log_tpl = '\{"node":"{node|short}","rev":"{rev}","author":"{author|urlescape}","branch":"{branches}","parents":"{parents}","date":"{date|isodate}","tags":"{tags}","desc":"{desc|urlescape}\"}\n'
+
+    def revision(self, identifier):
+        """Get the identified revision as a Revision object"""
+
+        out = self.hg_log(identifier=str(identifier), 
+                                     template=self.rev_log_tpl)
+                
+        return Revision(out)   
+
+    def revisions(self, slice_):
+        """Retruns a list of Revision objects for the given slice"""
+        out = self.hg_log(identifier=":".join([str(x)for x in (slice_.start, slice_.stop)]), 
+                                             template=self.rev_log_tpl)
+                        
+        revs = []
+        for entry in out.split('\n')[:-1]:
+            revs.append(Revision(entry))
+
+        return revs      
+    
+    def read_config(self):
+        """Read the configuration as seen with 'hg showconfig'
+        Is called by __init__ - only needs to be called explicitly
+        to reflect changes made since instantiation"""
+        res = self.hg_command("showconfig")
+        cfg = {}
+        for row in res.split("\n"):
+            section, ign, value = row.partition("=")
+            main, ign, sub = section.partition(".")
+            sect_cfg = cfg.setdefault(main, {})
+            sect_cfg[sub] = value.strip()
+        self.cfg = cfg
+        return cfg
+
+    def config(self, section, key):
+        """Return the value of a configuration variable"""
+        if not self.cfg: 
+            self.cfg = self.read_config()
+        return self.cfg.get(section, {}).get(key, None)
+    
+    def configbool(self, section, key):
+        """Return a config value as a boolean value.
+        Empty values, the string 'false' (any capitalization),
+        and '0' are considered False, anything else True"""
+        if not self.cfg: 
+            self.cfg = self.read_config()
+        value = self.cfg.get(section, {}).get(key, None)
+        if not value: 
+            return False
+        if (value == "0" 
+            or value.upper() == "FALSE"
+            or value.upper() == "None"): 
+            return False
+        return True
+
+    def configlist(self, section, key):
+        """Return a config value as a list; will try to create a list
+        delimited by commas, or whitespace if no commas are present"""
+        if not self.cfg: 
+            self.cfg = self.read_config()
+        value = self.cfg.get(section, {}).get(key, None)
+        if not value: 
+            return []
+        if value.count(","):
+            return value.split(",")
+        else:
+            return value.split()
